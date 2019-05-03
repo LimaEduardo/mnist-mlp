66b1bd62169f878ff100fef88c34d32462b3bdcf
Thuza <thuzax>
2019-05-03 13:06:49 -0300

    resultados dos testes

--- mnist-nosso.py
+++ mnist-nosso.py
@@ -3,6 +3,8 @@ import math
 import copy
 import warnings
 import numpy as np
+import json
+import time
 
 from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import fetch_mldata
@@ -120,17 +122,29 @@ if __name__=="__main__":
 
 
 
+    output_data = {}
 
     train_set = data_train
     train_result = target_train
 
+
     test_set = data_test
     test_result = target_test
 
+
+
+
+
+
     # Define parameter
-    alpha = 0.01
-    epoch = 500
-    neurons = [64, 30, 10] # number of neurons each layer
+    alpha = 0.07
+    epoch = 100
+    neurons = [64, 128, 10] # number of neurons each layer
+
+
+
+
+
 
 
     # Initiate weight and bias with 0 value
@@ -179,7 +193,10 @@ if __name__=="__main__":
             weight_2[i][j] = 2 * random.random() - 1
 
 
-    for e in range(epoch):
+    e = 0
+    has_time = True
+    inicio = time.time()
+    while(e < epoch and has_time):
         cost_total = 0
         for idx, data_list in enumerate(train_set): # Update for each data; SGD
             
@@ -233,29 +250,91 @@ if __name__=="__main__":
                     weight[i][j] -=  alpha * (delta_1[j] * data_list[i])
                     bias[j] -= alpha * delta_1[j]
         
-
+            
         cost_total /= len(train_set)
         if(e % 100 == 0):
             print(cost_total)
+        
+        fim = time.time()
+        if ((fim - inicio) > (3600 * 1.25)):
+            has_time = False
 
+        e += 1
+
+    
+    print(fim, inicio, fim - inicio)
     res = matrix_mul_bias(test_set, weight, bias)
     res_2 = matrix_mul_bias(res, weight_2, bias_2)
+
 
     # Get prediction
     preds = []
     for r in res_2:
         preds.append(max(enumerate(r), key=lambda x:x[1])[0])
 
+
+    # print(output_data["confusion_matrix"])
+
+
     for i in range(len(test_result)):
         test_result[i] = int(test_result[i])
     # Print prediction
-    print("Resultado esperado: ", test_result)
+    print("Resultado esperado: ", list(test_result))
     print("Predição:", preds)
-
 
     # Calculate accuration
     acc = 0.0
     for i in range(len(preds)):
         if preds[i] == int(test_result[i]):
             acc += 1
-    print(acc / len(preds) * 100, "%")
+    acc = acc / len(preds)
+    print(acc * 100, "%")
+
+
+
+
+    output_data["total_training_time"] = fim - inicio
+    fim = time.time()
+
+    output_data["total_time"] = fim - inicio
+
+    output_data["len_train"] = len(train_set)
+    output_data["len_test"] = len(test_set)
+
+
+    output_data["total_epochs"] = e
+    output_data["weights_1"] = {}
+    for pos, w in enumerate(weight):
+        output_data["weights_1"][str(pos)] = w
+    
+
+
+    output_data["weights_2"] = {}
+    for pos, w in enumerate(weight_2):
+        output_data["weights_2"][str(pos)] = w
+    
+    output_data["bias_1"] = {}
+    for pos, b in enumerate(bias):
+        output_data["bias_1"][str(pos)] = b
+    
+    output_data["bias_2"] = {}
+    for pos, b in enumerate(bias):
+        output_data["bias_2"][str(pos)] = b
+    
+    output_data["predictions"] = preds
+    output_data["target"] = list(test_result)
+    cm = confusion_matrix(test_result,preds)
+    output_data["confusion_matrix"] = {}
+
+    for pos, item in enumerate(cm):
+        output_data["confusion_matrix"][str(pos)] = []
+        for i in range(len(item)):
+            output_data["confusion_matrix"][str(pos)].append(int(item[i]))
+
+    #     output_data["confusion_matrix"][str(pos)] = list(item)
+    output_data["acc"] = acc
+
+    # print(output_data)
+
+    with open("resultados.json", "w") as output_file:
+        output_file.write(json.dumps(output_data, indent=2))
